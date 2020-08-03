68d417b6bad921a6d8f33b1a3465deeb6f639811
Doris Mondo <doris.ekwe@yahoo.com>
2020-08-03 16:35:56 -0400

    update

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let main = document.querySelector("main")
 
-
-class traveler {
+class Traveler {
     constructor(name, food) {
         this.name = name
         this.food = 1
@@ -22,82 +21,99 @@ class traveler {
             this.food -= 1
 
         }
-        console.log(this.eat)
     }
 
 }
-class Doctor extends traveler {
-    construction(name, food, heal) {
-        super(name, food)
-        this.heal =
-            this.isHealthy = true
+class Doctor extends Traveler {
+    constructor(name) {
+        super(name)
 
     }
+
+    heal(traveler) {
+        traveler.isHealthy = true
+    }
 }
-class Hunter extends traveler {
-    construction(name, food, hunt, eat, giveFood) {
-        super(name, food)
+class Hunter extends Traveler {
+    constructor(name) {
+        super(name)
         this.food = 2
-        this.hunt =
-            this.eat =
-            giveFood(traveler, numOfFoodUnits)
+
+    }
+    eat() {
+        if (this.food > 1) {
+            this.food -= 2
+
+        } else {
+            this.food = 0
+            this.isHealthy = false
+        }
+
+    }
+    giveFood(traveler, numOfFoodUnits) {
+        if (this.food >= numOfFoodUnits) {
+            this.food -= numOfFoodUnits
+            traveler.food += numOfFoodUnits
+        }
+
+    }
+
+    hunt() {
+        this.food += 5
+
     }
 }
 
 main.append();
 
-let button1Element = document.createElement('button1')
-let button2Element = document.createElement("button2")
-let button3Element = document.createElement("button3")
+let button1Element = document.createElement('button')
+let button2Element = document.createElement("button")
 let inputElement = document.createElement('input')
-document.querySelector("button1")
-document.querySelector("buttons2")
-document.querySelector("button3")
-document.querySelector("input")
 
-main.appendChild('button1')
-main.appendChild('button2')
-main.appendChild('button3')
-main.appendChild('input')
+main.appendChild(button1Element)
+main.appendChild(button2Element)
+main.appendChild(inputElement)
 
-button1Name = feed
-button2Name = add
-button3Name =
-    class Wagon {
-        constructor(capacity) {
-            this.capacity = capacity
-            this.passengers = []
+button1Element.append('feed')
+button2Element.append('add')
 
-        }
-
-        getAvailableSeatCount() {
-            return this.capacity - this.passengers.length
-
-        }
-
-        join(traveler) {
-            if (this.getAvailableSeatCount() >= 1) {
-                this.passengers.push(traveler)
-            }
-        }
-
-        shouldQuarantine() {
-            for (let index = 0; index < this.passengers.length; index++) {
-                var passenger = this.passengers[index]
-                if (passenger.isHealthy === false) {
-                    return true
-                }
-
-            }
-            return false
-        }
-        totalFood() {
-            for (let index = 0; index + this.food; index++) {
-                var food = this.food + hunt[index]
-
-                return this.totalFood
-            }
-
-        }
+class Wagon {
+    constructor(capacity) {
+        this.capacity = capacity
+        this.passengers = []
 
     }
+
+    getAvailableSeatCount() {
+        return this.capacity - this.passengers.length
+
+    }
+
+    join(traveler) {
+        if (this.getAvailableSeatCount() >= 1) {
+            this.passengers.push(traveler)
+        }
+    }
+
+    shouldQuarantine() {
+        for (let index = 0; index < this.passengers.length; index++) {
+            var passenger = this.passengers[index]
+            if (passenger.isHealthy === false) {
+                return true
+            }
+
+        }
+        return false
+    }
+    totalFood() {
+        var foodpassengers = 0
+
+        for (let index = 0; index < this.passengers.length; index++) {
+            var passenger = this.passengers[index]
+            foodpassengers += passenger.food
+
+        }
+        return foodpassengers
+    }
+
+}
